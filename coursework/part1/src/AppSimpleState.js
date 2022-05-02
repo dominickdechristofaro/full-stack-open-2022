1df78ceb319fd332feb384738ad6c4b122229e29
Dominick DeChristofaro <dominick.dechristofaro@gmail.com>
2022-05-01 23:22:19 -0400

    Refactored coursework sections in preparation for part_01_04 exercises

---
+++ coursework/part1/src/AppSimpleState.js
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+
+const Display = ({ counter} ) => {
+  return (
+    <div>{counter}</div>
+  )
+}
+
+const Button = ({ onClick, text }) => {
+  return (
+    <button onClick={onClick}>
+      {text}
+    </button>
+  )
+}
+
+const AppSimpleState = () => {
+  const [ counter, setCounter ] = useState(0)
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
+
+  return (
+    <div>
+      <Display counter={counter}></Display>
+      <Button
+        onClick={increaseByOne}
+        text='plus'
+      />
+      <Button
+        onClick={setToZero}
+        text='zero'
+      />
+      <Button
+        onClick={decreaseByOne}
+        text='minus'
+      />
+    </div>
+  )
+}
+
+export default AppSimpleState
--- coursework/part1/src/index.js
+++ coursework/part1/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import App from './App'
+import AppSimpleState from './AppSimpleState'
+import AppComplexState from './AppComplexState'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById('root')).render(<AppComplexState />)
